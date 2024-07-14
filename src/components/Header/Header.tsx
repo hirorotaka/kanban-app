cad464893a431f9043e33c12199fa2f43b797ef2
hiroki takada <hirorotaka5@gmail.com>
2024-07-14 15:28:07 +0900

    ヘッダーの文字編集機能の修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,13 @@
-import { CustomErrorMessage } from '../CustomErrorMessage/CustomErrorMessage';
 import { useHeader } from '../../hooks/useHeader';
-import { InputForm } from '../UI/Form/InputForm';
 
 export const Header = () => {
   const {
     selectedItem,
-    editMode,
-    handleClick,
-    handleTextareaChange,
-    handleSubmit,
-    onSubmit,
-    register,
-    trigger,
-    errors,
+    isEditing,
+    handleChange,
+    handleHeaderChange,
+    handleOnClick,
+    editValue,
   } = useHeader();
 
   if (!selectedItem) {
@@ -20,28 +15,28 @@ export const Header = () => {
   }
 
   return (
-    <header className="relative bg-white p-4 shadow-lg transition-shadow">
-      <div className="flex items-center">
+    <header className="bg-white p-4 shadow-lg transition-shadow">
+      <div className="flex  items-center ">
         <span className="mr-2 text-2xl text-blue-500">{selectedItem.icon}</span>
-        <div onClick={handleClick} className="relative flex items-center">
-          <h1 className="text-2xl font-bold">{selectedItem.label}</h1>
-          {editMode && (
-            <div className="animate-bounce-in absolute left-0 top-[80%] z-50 mt-1 rounded-lg border border-gray-300 bg-white p-1 text-gray-500 shadow-2xl shadow-gray-950">
-              <InputForm
-                formUi="w-[600px] text-base"
-                handleSubmit={handleSubmit}
-                onSubmit={onSubmit}
-                register={register}
-                trigger={trigger}
-                errors={errors}
-                handleTextareaChange={handleTextareaChange}
-              />
-              {errors.label?.message && (
-                <CustomErrorMessage message={errors.label.message} />
-              )}
-            </div>
-          )}
-        </div>
+        {isEditing ? (
+          <input
+            type="text"
+            value={editValue}
+            onChange={handleChange}
+            onBlur={handleHeaderChange}
+            autoFocus
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleHeaderChange();
+              }
+            }}
+            className="w-3/4 rounded border border-none text-2xl font-bold caret-blue-500 outline-none"
+          />
+        ) : (
+          <span onClick={handleOnClick} className="text-2xl font-bold">
+            {selectedItem.label}
+          </span>
+        )}
       </div>
     </header>
   );
--- src/hooks/useHeader.ts
+++ src/hooks/useHeader.ts
@@ -1,108 +1,44 @@
-import { zodResolver } from '@hookform/resolvers/zod';
 import { useContext, useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import * as z from 'zod';
 import { NavItemContext } from '../context/NavItemContext';
-
-const varidationMaxlength = 30;
-
-// バリデーションスキーマの定義
-const validationSchema = z.object({
-  label: z
-    .string()
-    .min(1, `1~${varidationMaxlength}文字以内で入力してください`)
-    .max(
-      varidationMaxlength,
-      `1~${varidationMaxlength}文字以内で入力してください`
-    )
-    .refine((value) => value.trim().length > 0, '空白文字のみは入力できません'),
-});
-
-// バリデーションスキーマから型を推論
-export type FormValues = z.infer<typeof validationSchema>;
 
 export const useHeader = () => {
   const { navItems, navCheckId, updateNavItem } =
     useContext(NavItemContext) || {};
   const selectedItem = navItems?.find((item) => item.id === navCheckId);
 
-  const [editMode, setEditMode] = useState(false);
-  const [prevLabel, setPrevLabel] = useState(selectedItem?.label || '');
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-    reset,
-    setValue,
-    clearErrors,
-    getValues,
-    trigger,
-    watch,
-  } = useForm<FormValues>({
-    defaultValues: { label: selectedItem?.label || '' },
-    resolver: zodResolver(validationSchema),
-    mode: 'onChange',
-  });
+  const [editValue, setEditValue] = useState(selectedItem?.label || '');
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
-    const subscription = watch((value, { name }) => {
-      if (
-        name === 'label' &&
-        value.label &&
-        value.label.length >= varidationMaxlength
-      ) {
-        setValue('label', value.label.slice(0, varidationMaxlength));
-      }
-    });
-    return () => subscription.unsubscribe();
-  }, [watch, setValue]);
+    setEditValue(selectedItem?.label || '');
+  }, [selectedItem]);
 
-  useEffect(() => {
-    if (selectedItem) {
-      reset({ label: selectedItem.label });
-      setPrevLabel(selectedItem.label);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditValue(e.target.value);
+  };
+
+  const handleHeaderChange = () => {
+    if (!selectedItem || !updateNavItem) return;
+    if (editValue.trim() === '') {
+      setEditValue('無題');
+      updateNavItem(selectedItem.id, '無題');
+    } else {
+      setEditValue(editValue);
+      updateNavItem(selectedItem.id, editValue);
     }
-  }, [selectedItem, reset]);
-
-  const handleUpdateNavItem = (label: string) => {
-    if (!updateNavItem || !selectedItem) return;
-    updateNavItem(selectedItem.id, label);
-    setPrevLabel(label);
+    setIsEditing(false);
   };
 
-  const onSubmit = (data: FormValues) => {
-    handleUpdateNavItem(data.label);
-    setEditMode(false);
-  };
-
-  const handleTextareaChange = () => {
-    const currentLabel = getValues('label');
-
-    if (isValid && currentLabel !== prevLabel) {
-      handleUpdateNavItem(currentLabel);
-    } else if (!isValid) {
-      setValue('label', prevLabel);
-    }
-    setEditMode(false);
-    clearErrors('label');
-  };
-
-  const handleClick = () => {
-    setEditMode(true);
-    clearErrors('label');
+  const handleOnClick = () => {
+    setIsEditing(true);
   };
 
   return {
     selectedItem,
-    editMode,
-    errors,
-    isValid,
-    register,
-    trigger,
-    handleSubmit,
-    onSubmit,
-    handleTextareaChange,
-    handleClick,
+    isEditing,
+    handleChange,
+    handleHeaderChange,
+    handleOnClick,
+    editValue,
   };
 };
