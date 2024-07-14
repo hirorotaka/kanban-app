9ea5462f446166ff5fac8691bd219730459ed63b
hiroki takada <hirorotaka5@gmail.com>
2024-07-14 15:17:42 +0900

    タスクカードのレイアウト調整

--- src/components/kanban/TaskCard.tsx
+++ src/components/kanban/TaskCard.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 import { TaskProps } from '../../types/type';
-import { IoApps, IoTrashOutline } from 'react-icons/io5';
+import { IoApps, IoPencil, IoTrashOutline } from 'react-icons/io5';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { BoardContext } from '../../context/BoardContext';
@@ -68,7 +68,7 @@ export const CardTask = ({ task, newTaskId }: TaskProps) => {
       <div
         ref={setNodeRef}
         style={style}
-        className="relative flex h-full min-h-[120px] cursor-grab items-center rounded-xl border-4 border-blue-500 bg-white p-2.5 text-left shadow-xl"
+        className="relative flex min-h-[120px] cursor-grab items-center rounded-xl border-2 border-blue-500 bg-white p-2.5 text-left shadow-xl"
       />
     );
   }
@@ -77,36 +77,56 @@ export const CardTask = ({ task, newTaskId }: TaskProps) => {
     <div
       ref={setNodeRef}
       style={style}
-      className={`mb-4 rounded-xl border-2 bg-blue-100 p-2 shadow-md hover:shadow-xl ${highlighted ? 'animate-highlight' : 'border-transparent'}`}
+      className={`mb-1 rounded-xl border-2 bg-blue-100 p-1 pb-3 shadow-lg ${editMode ? '' : 'hover:border-blue-500'} ${highlighted ? 'animate-highlight' : 'border-transparent'}`}
       onMouseEnter={() => setMouseIsOver(true)}
       onMouseLeave={() => setMouseIsOver(false)}
     >
-      <div className="flex items-center justify-between">
-        <div className="flex min-h-[60px] w-[220px] items-center">
-          {/* ドラッグアイコン表示 */}
+      <div className="flex flex-col">
+        {/* ドラッグアイコンを上部に移動 */}
+        <div className="flex min-h-9 justify-between">
           <button
             {...attributes}
             {...listeners}
-            className={`mx-2 text-gray-500 focus:outline-none ${
+            className={`text-gray-500 focus:outline-none ${
               isGrabbing ? 'cursor-grabbing' : 'cursor-grab'
             }`}
             onMouseDown={() => setIsGrabbing(true)}
             onMouseUp={() => setIsGrabbing(false)}
           >
-            <IoApps size={16} />
+            <IoApps size={18} />
           </button>
+          {/* タスク削除アイコンの表示 */}
+          <div className={`flex ${!mouseIsOver && !editMode && 'hidden'}`}>
+            <button
+              onClick={toggleEditMode}
+              className={`rounded px-1 py-2 text-gray-500 transition duration-200 ease-in-out hover:text-red-500`}
+            >
+              <IoPencil size={18} />
+            </button>
+            <button
+              onClick={() => {
+                if (!deleteTask) return;
+                deleteTask(task.id);
+              }}
+              className={`rounded px-1 py-2 text-gray-500 transition duration-200 ease-in-out hover:text-red-500`}
+            >
+              <IoTrashOutline size={18} />
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-between">
           {/* タスクの内容表示・編集機能 */}
-          <div onClick={() => setEditMode(true)} className="grow font-bold">
+          <div className="grow">
             {!editMode ? (
               <p
-                className="my-auto min-h-[100px] overflow-y-auto overflow-x-hidden whitespace-pre-wrap break-words px-2 py-1"
-                style={{ lineHeight: '1.5' }}
+                className=" size-full overflow-y-auto overflow-x-hidden  whitespace-pre-wrap break-words px-2 text-base"
+                style={{ wordBreak: 'break-word' }}
               >
                 {task.content}
               </p>
             ) : (
               <textarea
-                className="min-h-[100px] w-full resize-none whitespace-pre-wrap break-words rounded border px-2 py-1 outline-none focus:border-blue-500"
+                className="w-full resize-none whitespace-pre-wrap break-words rounded border-2 py-1 text-base outline-none focus:border-blue-500"
                 value={task.content}
                 onChange={(e) => {
                   if (!updateTask) return;
@@ -123,27 +143,11 @@ export const CardTask = ({ task, newTaskId }: TaskProps) => {
                     toggleEditMode();
                   }
                 }}
-                style={{ lineHeight: '1.5' }}
+                style={{ lineHeight: '1.5', minHeight: '100px' }}
               />
             )}
           </div>
         </div>
-        {/* タスク削除アイコンの表示 */}
-        {mouseIsOver && !editMode ? (
-          <div className="min-w-7">
-            <button
-              onClick={() => {
-                if (!deleteTask) return;
-                deleteTask(task.id);
-              }}
-              className={`rounded px-1 py-2 text-gray-500 transition duration-200 ease-in-out hover:text-red-500`}
-            >
-              <IoTrashOutline size={20} />
-            </button>
-          </div>
-        ) : (
-          <div className="min-w-7" />
-        )}
       </div>
     </div>
   );
